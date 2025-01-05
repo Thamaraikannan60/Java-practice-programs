7b39f2f80a7d0c7b4d0542effbb0d9a7147f933d
Thamaraikannan60 <thamaraikannan135648@gmail.com>
2025-01-05 17:32:18 +0530

    Update FibonacciSeries.java

--- src/jan1/FibonacciSeries.java
+++ src/jan1/FibonacciSeries.java
@@ -12,11 +12,11 @@ public class FibonacciSeries {
 		int i;
 		int num1=0;
 		int num2=1;
-		int res;
+		
 		System.out.println("The first "+num+" fibonacci series  is");
 		for(i=1;num>=i;i++) {
 			System.out.println(num1);
-			res = num1+num2;
+			int res = num1+num2;
 			num1=num2;
 			num2=res;
 		}
